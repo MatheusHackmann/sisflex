87d93d733afbc65ce3acbe7e874afc5018afca74
Matheus Hackmann <mthshackmann20@gmail.com>
2018-09-04 14:35:50 -0300

    Ultimas atualizacoes

--- classes/Alimentos.class.php
+++ classes/Alimentos.class.php
@@ -29,5 +29,95 @@ class Alimentos {
 		return $results;
 	}
 
+	public function calcularNovaPorcao($alimento = 'Iogurte', $carb = 9.1, $prot = 6.8, $gord = 7, $porcao = 170, $novaPorcao = 400){
+
+	//Calculando as calorias que cada macro possui no alimento
+		$kcalCarb = $carb*4;
+		$kcalProt = $prot*4;
+		$kcalGord = $gord*9;
+
+	//Calculando as calorias totais, somando as calorias de cada macro
+		$kcals = number_format(($kcalCarb + $kcalProt + $kcalGord), 1, '.', '');
+
+	//Calculando as calorais por 1 g da porcao
+		$kcalPorcao = number_format(($kcals / $porcao), 2, '.', '');
+
+	//Calcula a porcentagem de cada macro na porção
+		$porcentagemCarb = number_format(($kcalCarb/$kcals)*100, 2, '.', '');
+		$porcentagemProt = number_format(($kcalProt/$kcals)*100, 2, '.', '');
+		$porcentagemGord = number_format(($kcalGord/$kcals)*100, 2, '.', '');	
+
+		echo "
+		<pre>
+		Informações do alimento por porção
+
+		<b>Alimento:</b> $alimento
+		<b>Carboidrato:</b> $kcalCarb"." kcal / $carb"."g / $porcentagemCarb"."%
+		<b>Proteína:</b> $kcalProt"." kcal / $prot"."g / $porcentagemProt"."%
+		<b>Gordura:</b> $kcalGord"." kcal  / $gord"."g / $porcentagemGord"."%
+
+		<b>KCALS:</b> $kcals
+		<b>KCALS/g:</b> $kcalPorcao
+		-------------------------------------------------
+		</pre>";
+
+	//Calcula as novas calorias multiplicando a Nova Porção pela Kcal/Porção
+		$novaKcal = $kcalPorcao*$novaPorcao;
+
+	//Porcentagem das novas calorias
+		$porcentagemNovaKcal = number_format(($novaKcal/$kcals)*100, 2, '.', '');
+
+		if ($porcentagemNovaKcal > 100) {
+		//Calcula a porcentagem de adição de kcal
+			$porcentagemPorcao = $porcentagemNovaKcal - 100;
+
+			$carb = number_format((($porcentagemPorcao/100)*$carb) + $carb, 1, '.', '');
+			$prot = number_format((($porcentagemPorcao/100)*$prot) + $prot, 1, '.', '');
+			$gord = number_format((($porcentagemPorcao/100)*$gord) + $gord, 1, '.', '');
+
+			$kcalCarb = $carb*4;
+			$kcalProt = $prot*4;
+			$kcalGord = $gord*9;		
+
+			echo "<pre> 	
+			Novas informações do alimento por porção
+
+			<b>Alimento:</b> $alimento
+			<b>Carboidrato:</b> $kcalCarb"." kcal / $carb"."g
+			<b>Proteína:</b> $kcalProt"." kcal / $prot"."g
+			<b>Gordura:</b> $kcalGord"." kcal  / $gord"."g
+
+			<b>KCALS:</b> $novaKcal
+			-------------------------------------------------------------
+			</pre>";		
+		}
+		else if ($porcentagemNovaKcal < 100){
+		//Calcula a porcentagem de adição de kcal
+			$porcentagemPorcao = abs($porcentagemNovaKcal - 100);
+
+			echo $porcentagemPorcao;
+
+			$carb = abs(number_format((($porcentagemPorcao/100)*$carb) - $carb, 1, '.', ''));
+			$prot = abs(number_format((($porcentagemPorcao/100)*$prot) - $prot, 1, '.', ''));
+			$gord = abs(number_format((($porcentagemPorcao/100)*$gord) - $gord, 1, '.', ''));
+
+			$kcalCarb = $carb*4;
+			$kcalProt = $prot*4;
+			$kcalGord = $gord*9;		
+
+			echo "<pre> 	
+			Novas informações do alimento por porção
+
+			<b>Alimento:</b> $alimento
+			<b>Carboidrato:</b> $kcalCarb"." kcal / $carb"."g
+			<b>Proteína:</b> $kcalProt"." kcal / $prot"."g
+			<b>Gordura:</b> $kcalGord"." kcal  / $gord"."g
+
+			<b>KCALS:</b> $novaKcal
+			-------------------------------------------------------------
+			</pre>";		
+		}		
+	}
+
 }
 ?>
---
+++ diario.php
@@ -0,0 +1,224 @@
+<style>
+	/*.table {
+		max-width: none !important;
+	}*/
+</style>
+<div class="container">
+	<div class="row">
+		<div class="ol-12">
+			
+			<div class="row" style="border: 1px solid #f2f2f2; border-radius: 5px;">
+				<div class="col-12 col-md-2" style="border: 1px solid #999999; border-radius: 5px;">
+					<span class="fas fa-arrow-circle-left"></span>
+				</div>
+				<div class="col-12 col-md-8 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+					<p>Terça-feira, 21 de agosto de 2018</p>
+				</div>
+				<div class="col-12 col-md-2 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+					<span class="fas fa-arrow-circle-right"></span>
+				</div>
+			</div>
+
+			<div class="row" style="border: 1px solid #f2f2f2; border-radius: 5px; margin-top: 1%;">
+				<div class="col-12 col-md-6" style="border: 1px solid #999999; border-radius: 5px;">
+					<div class="row">
+						<div class="col-12 col-md-4 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+							<label for="">Meta</label>
+							<p>2.000</p>
+						</div>
+						<div class="col-12 col-md-4 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+							<label for="">Atingido</label>
+							<p>1.700</p>						
+						</div>
+						<div class="col-12 col-md-4 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+							<label for="">Restante</label>
+							<p>300</p>						
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-6" style="border: 1px solid #999999; border-radius: 5px;">
+					<div class="row">
+						<div class="col-12 col-md-4 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>C</p>
+							<span class="fas fa-chart-pie"></span>
+							<p>130/200</p>
+						</div>
+						<div class="col-12 col-md-4 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>P</p>
+							<span class="fas fa-chart-pie"></span>
+							<p>100/150</p>
+						</div>							
+						<div class="col-12 col-md-4 text-center" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>G</p>
+							<span class="fas fa-chart-pie"></span>
+							<p>40/75</p>
+						</div>							
+					</div>
+				</div>
+			</div>
+
+			<!-- TABLE -->
+			<div class="row" style="margin-top: 5%;">
+				<div class="col-12" style="border: 1px solid #999999; border-radius: 5px;">
+					<table class="table">
+						<thead>
+							<tr>
+								<th></th>
+								<th style="width: 50%;">Refeição 1</th>
+								<th>C</th>
+								<th>P</th>
+								<th>G</th>
+								<th>F</th>
+								<th>KCAL</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><span class="fas fa-edit"></span></td>
+								<td>Food - 1un</td>
+								<td>9</td>
+								<td>7</td>
+								<td>7</td>
+								<td>0</td>
+								<td>127</td>
+								<td><span class="fas fa-trash-alt"></span></td>
+							</tr>
+							<tr>
+								<td><span class="fas fa-edit"></span></td>
+								<td>Food - 100g</td>
+								<td>0</td>
+								<td>23</td>
+								<td>1</td>
+								<td>0</td>
+								<td>101</td>
+								<td><span class="fas fa-trash-alt"></span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-12" style="border: 1px solid #999999; border-radius: 5px;">
+					<div class="row">
+						<div class="col-12 col-md-6 text-left" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>META: 800kcal</p>
+						</div>
+						<div class="col-12 col-md-6 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>TOTAL: 228kcal</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+					<button class="btn btn-primary">+ Add food</button>
+				</div>				
+			</div>
+
+			<!-- TABLE -->
+			<div class="row" style="margin-top: 2%;">
+				<div class="col-12" style="border: 1px solid #999999; border-radius: 5px;">
+					<table class="table">
+						<thead>
+							<tr>
+								<th></th>
+								<th style="width: 50%;">Refeição 2</th>
+								<th>C</th>
+								<th>P</th>
+								<th>G</th>
+								<th>F</th>
+								<th>KCAL</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><span class="fas fa-edit"></span></td>
+								<td>Food - 1un</td>
+								<td>9</td>
+								<td>7</td>
+								<td>7</td>
+								<td>0</td>
+								<td>127</td>
+								<td><span class="fas fa-trash-alt"></span></td>
+							</tr>
+							<tr>
+								<td><span class="fas fa-edit"></span></td>
+								<td>Food - 100g</td>
+								<td>0</td>
+								<td>23</td>
+								<td>1</td>
+								<td>0</td>
+								<td>101</td>
+								<td><span class="fas fa-trash-alt"></span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-12" style="border: 1px solid #999999; border-radius: 5px;">
+					<div class="row">
+						<div class="col-12 col-md-6 text-left" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>META: 800kcal</p>
+						</div>
+						<div class="col-12 col-md-6 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>TOTAL: 228kcal</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+					<button class="btn btn-primary">+ Add food</button>
+				</div>				
+			</div>
+
+			<!-- TABLE -->
+			<div class="row" style="margin-top: 2%;">
+				<div class="col-12" style="border: 1px solid #999999; border-radius: 5px;">
+					<div class="row">
+						<div class="col-12 col-md-1 text-center"><span class="fas fa-edit"></span></div>
+						<div class="col-12 col-md-5 text-center" style="border-left: 1px solid #999999; border-right: 1px solid #999999;">Food - 1un</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">C  9</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">P  7</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">G  7</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">F  0</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">K 127</div>
+						<div class="col-12 col-md-1 text-center"><span class="fas fa-trash-alt"></span></div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-12 col-md-1 text-center"><span class="fas fa-edit"></span></div>
+						<div class="col-12 col-md-5 text-center" style="border-left: 1px solid #999999; border-right: 1px solid #999999;">Food - 1un</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">C  9</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">P  7</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">G  7</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">F  0</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">K 127</div>
+						<div class="col-12 col-md-1 text-center"><span class="fas fa-trash-alt"></span></div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-12 col-md-1 text-center"><span class="fas fa-edit"></span></div>
+						<div class="col-12 col-md-5 text-center" style="border-left: 1px solid #999999; border-right: 1px solid #999999;">Food - 1un</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">C  9</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">P  7</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">G  7</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">F  0</div>
+						<div class="col-12 col-md-1 text-center" style="border-right: 1px solid #999999;">K 127</div>
+						<div class="col-12 col-md-1 text-center"><span class="fas fa-trash-alt"></span></div>
+					</div>
+															
+				</div>
+				<div class="col-12" style="border: 1px solid #999999; border-radius: 5px;">
+					<div class="row">
+						<div class="col-12 col-md-6 text-left" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>META: 800kcal</p>
+						</div>
+						<div class="col-12 col-md-6 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+							<p>TOTAL: 228kcal</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 text-right" style="border: 1px solid #999999; border-radius: 5px;">
+					<button class="btn btn-primary">+ Add food</button>
+				</div>				
+			</div>						
+
+		</div>
+	</div>
+</div>
